dc0bf24375ea7e4e8c9daa1ad6dfb21cbe14665e
esraaahmed422 <asraaljmal9@gmail.com>
2023-09-13 15:06:23 +0300

    second commit

---
+++ assets/script.js
@@ -0,0 +1,56 @@
+/* Initialize Swiper */
+document.querySelector('#search-icon').onclick = () =>{
+document.querySelector('#search-form').classList.toggle('active')
+}
+  
+document.querySelector('#close').onclick = () =>{
+document.querySelector('#search-form').classList.remove('active')
+}
+  
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true,
+});
+
+var swiper = new Swiper(".Events-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+  },
+  breakpoints: {
+      640: {
+          slidesPerView: 1,
+      },
+      768: {
+          slidesPerView: 2,
+      },
+      1024: {
+          slidesPerView: 3,
+      },
+  },
+});

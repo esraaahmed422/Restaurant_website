13a7859e7ee8823adf9713a4478ed395528dcde7
Esraa Ahmed <123988592+esraaahmed422@users.noreply.github.com>
2023-09-15 01:56:08 +0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,51 @@
-/* Initialize Swiper */
+// search event
 document.querySelector('#search-icon').onclick = () =>{
-document.querySelector('#search-form').classList.toggle('active')
+  document.querySelector('#search-form').classList.toggle('active')
 }
-  
+
 document.querySelector('#close').onclick = () =>{
-document.querySelector('#search-form').classList.remove('active')
+  document.querySelector('#search-form').classList.remove('active')
 }
-  
+
+
+// navbar variables
+const nav = document.querySelector('.navbar');
+const bars = document.querySelector('#bars');
+const cart = document.querySelector('.fa-shopping-cart');
+const shoppingCart = document.querySelector('.cart-box');
+
+
+// nav  function
+const navToggle = function () {
+  nav.classList.toggle('active');
+  bars.classList.toggle('active');
+}
+
+// shopping-cart toggle function
+const cartToggle = function () { 
+  shoppingCart.classList.toggle('active') 
+}
+
+// add event on cart-toggle-btn
+cart.addEventListener('click', function () {
+
+  // If the navbar-nav has an "active" class, it will be "removed".
+  if (nav.classList.contains('active')) navToggle();
+  cartToggle();
+
+});
+
+// add event on nav-toggle-btn
+bars.addEventListener('click', function () {
+
+  // If the shopping-cart has an "active" class, it will be "removed".
+  if (shoppingCart.classList.contains('active')) cartToggle();
+  navToggle();
+
+});
+
+
+// Initialize Swiper
 var swiper = new Swiper(".home-slider", {
   spaceBetween: 30,
   centeredSlides: true,
